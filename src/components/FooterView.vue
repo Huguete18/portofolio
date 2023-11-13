0e2ea10bfbb92a94d6fbfc379aa7357a177ce808
Huguete18 <serhuguet@protonmail.com>
2023-11-13 20:33:43 +0100

    Se agrega el mensaje en el main

--- src/components/FooterView.vue
+++ src/components/FooterView.vue
@@ -1,26 +1,55 @@
 <template>
-    <v-footer>
-        <v-card elevation="0" rounded="0" width="100%" class="bg-grey text-center">
-            <v-card-text>
-                <v-btn class="mw-4" icon="fas fa-comment" variant="plain" color="white"></v-btn>
-                <v-btn class="mw-4" icon="fas fa-university" variant="plain" color="white"></v-btn>
-                <v-btn class="mw-4" icon="fas fa-wifi" variant="plain" color="white"></v-btn>
-                <v-btn class="mw-4" icon="fas fa-podcast" variant="plain" color="white"></v-btn>
-            </v-card-text>
-            <v-card-text class="text-white">
-                @{{ new Date().getFullYear() }} -
-                <strong>Sergi Huguet Vazquez All Rights Reserved</strong>
-            </v-card-text>
-        </v-card>
-    </v-footer>
-</template>
+    <v-bottom-sheet v-model="sheet">
+      <template v-slot:activator="{ props }">
+        <div class="text-center">
+          <v-btn v-bind="props" color="purple" size="x-large" text="Click Me"></v-btn>
+        </div>
+      </template>
+  
+      <v-list>
+        <v-list-subheader>Open in</v-list-subheader>
+  
+        <v-list-item v-for="tile in tiles" :key="tile.title" :prepend-avatar="`/img/${tile.img}`" :title="tile.title" @click="openLink(tile.link)"></v-list-item>
+      </v-list>
+    </v-bottom-sheet>
+  </template>
+
 <script>
-export default {
-    
-}
-</script>
-<style>
-    .v-footer {
-        padding: 1px 0 !important;
+    export default {
+        data: () => ({
+            sheet: false,
+            tiles: [{
+                img: 'github.png',
+                title: 'GitHub',
+                link: 'https://github.com/Huguete18',
+            }, {
+                img: 'linkedin.png',
+                title: 'LinKedin',
+                link: 'https://www.linkedin.com/in/sergi-huguet-986b3071/',
+            }, {
+                img: 'protonmail.png',
+                title: 'ProtonMail',
+                link: 'mailto:serhuguet@protonmail.com',
+                email: 'serhuguet@protonmail.com',
+            }, {
+                img: 'instagram.png',
+                title: 'Instagram',
+                link: 'https://www.instagram.com/huguete_18/',
+            }, {
+                img: 'whatsapp.png',
+                title: 'WhatsApp',
+                link: 'https://wa.me/34693783999',
+            }, ],
+        }),
+        methods: {
+            openLink(link) {
+            window.open(link, '_blank');
+            this.sheet = false;
+            },
+        },
     }
+</script>
+
+<style>
+
 </style>
